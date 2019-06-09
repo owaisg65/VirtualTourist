0146d19cdf971949e13bd10b91d7499a68406a6a
owais <owaisg65@gmail.com>
2019-06-09 14:35:20 +0300

    setup View Controller in main story borad

---
+++ VirtualTourist/Controller/PhotoAlbumViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by Owais Gaffas on 09/06/2019.
+//  Copyright © 2019 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class PhotoAlbumViewController : UIViewController {
+    
+}
